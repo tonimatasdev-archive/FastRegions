86cf4876b18d76b160014baab4602e5833104748
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-09-03 03:50:03 +0200

    Groovy to GradleDLS

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    java
+}
+
+val projectVersion: String by extra
+
+group = "net.tonimatasdev"
+version = projectVersion
+
+repositories {
+    mavenCentral()
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven(url = "https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+}
+
+val targetJavaVersion = 17
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks.withType<JavaCompile> {
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible()) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.withType<ProcessResources> {
+    inputs.property("version", version)
+
+    filesMatching("plugin.yml") {
+        expand("version" to version)
+    }
+}
