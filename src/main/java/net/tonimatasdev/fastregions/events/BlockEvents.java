e3bfe2f2e0c329c0f0b0dac43e0fa50daf76839f
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-09-09 20:22:12 +0200

    BLOCK_BREAK flag

--- src/main/java/net/tonimatasdev/fastregions/events/BlockEvents.java
+++ src/main/java/net/tonimatasdev/fastregions/events/BlockEvents.java
@@ -1,4 +1,16 @@
 package net.tonimatasdev.fastregions.events;
 
-public class BlockEvents {
+import net.tonimatasdev.fastregions.api.flag.Flag;
+import net.tonimatasdev.fastregions.api.region.Region;
+import net.tonimatasdev.fastregions.api.region.RegionManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BlockEvents implements Listener {
+    @EventHandler
+    private void onBlockBreak(BlockBreakEvent event) {
+        Region region = RegionManager.getRegion(event.getPlayer().getWorld(), event.getBlock().getLocation());
+        if (region != null && region.getFlags().contains(Flag.BLOCK_BREAK)) event.setCancelled(true);
+    }
 }
