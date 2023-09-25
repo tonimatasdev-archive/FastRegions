cecc1b8fb07af542226ea30d13b4b2011a1eafe5
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-09-25 17:45:58 +0200

    Fix/Add flags

--- src/main/java/net/tonimatasdev/fastregions/events/BlockEvents.java
+++ src/main/java/net/tonimatasdev/fastregions/events/BlockEvents.java
@@ -1,8 +1,7 @@
 package net.tonimatasdev.fastregions.events;
 
 import net.tonimatasdev.fastregions.api.flag.Flag;
-import net.tonimatasdev.fastregions.api.region.Region;
-import net.tonimatasdev.fastregions.api.region.RegionManager;
+import net.tonimatasdev.fastregions.util.EventUtils;
 import org.bukkit.block.data.type.Bed;
 import org.bukkit.block.data.type.RespawnAnchor;
 import org.bukkit.entity.Player;
@@ -14,138 +13,138 @@ import org.bukkit.event.block.*;
 public class BlockEvents implements Listener {
     @EventHandler
     private void onBreak(BlockBreakEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_break));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_break));
     }
 
     @EventHandler
     private void onPlace(BlockPlaceEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_place));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_place));
     }
 
     @EventHandler
     private void onMultiPlace(BlockMultiPlaceEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_place));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_place));
     }
 
     @EventHandler
     private void onTNTPrime(TNTPrimeEvent event) {
-        event.setCancelled(isCancelled(event, Flag.tnt_prime));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.tnt_prime));
     }
 
     @EventHandler
     private void onNotePlay(NotePlayEvent event) {
-        event.setCancelled(isCancelled(event, Flag.note_play));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.note_play));
     }
 
     @EventHandler
     private void onBurn(BlockBurnEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_burn));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_burn));
     }
 
     @EventHandler
     private void onSpread(BlockSpreadEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_spread));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_spread));
     }
 
     @EventHandler
     private void onLeavesDecay(LeavesDecayEvent event) {
-        event.setCancelled(isCancelled(event, Flag.leaves_decay));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.leaves_decay));
     }
 
     @EventHandler
     private void onSpongeAbsorb(SpongeAbsorbEvent event) {
-        event.setCancelled(isCancelled(event, Flag.sponge_absorb));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.sponge_absorb));
     }
 
     @EventHandler
     private void onGrow(BlockGrowEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_grow));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_grow));
     }
 
     @EventHandler
     private void onFertilize(BlockFertilizeEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_fertilize));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_fertilize));
     }
 
     @EventHandler
     private void onIgnite(BlockIgniteEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_ignite));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_ignite));
     }
 
     @EventHandler
     private void onSignChange(SignChangeEvent event) {
-        event.setCancelled(isCancelled(event, Flag.sign_change));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.sign_change));
     }
 
     @EventHandler
     private void onRedstone(BlockRedstoneEvent event) {
-        if (isCancelled(event, Flag.redstone)) event.setNewCurrent(0);
+        if (EventUtils.isBlockEventCancelled(event, Flag.redstone)) event.setNewCurrent(0);
     }
 
     @EventHandler
-    private void onPiston(BlockPistonEvent event) {
-        event.setCancelled(isCancelled(event, Flag.piston));
+    private void onPistonExtend(BlockPistonExtendEvent event) {
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.piston_extend));
+    }
+
+    @EventHandler
+    private void onPistonRetract(BlockPistonRetractEvent event) {
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.piston_retract));
     }
 
     @EventHandler
     private void onCauldronLevelChange(CauldronLevelChangeEvent event) {
-        event.setCancelled(isCancelled(event, Flag.cauldron_interact));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.cauldron_interact));
     }
 
     @EventHandler
     private void onEntityBlockForm(EntityBlockFormEvent event) {
         if (event.getEntity() instanceof Snowman) {
-            event.setCancelled(isCancelled(event, Flag.snowman_snow));
+            event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.snowman_snow));
         }
 
         if (event.getEntity() instanceof Player) {
-            event.setCancelled(isCancelled(event, Flag.frost_walker));
+            event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.frost_walker));
         }
     }
 
     @EventHandler
     private void onExplode(BlockExplodeEvent event) {
         if (event.getBlock().getBlockData() instanceof RespawnAnchor) {
-            event.setCancelled(isCancelled(event, Flag.respawn_anchor_explode));
+            event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.respawn_anchor_explode));
         }
 
         if (event.getBlock().getBlockData() instanceof Bed) {
-            event.setCancelled(isCancelled(event, Flag.bed_explode));
+            event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.bed_explode));
         }
     }
 
     @EventHandler
     private void onBellRing(BellRingEvent event) {
-        event.setCancelled(isCancelled(event, Flag.bell_ring));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.bell_ring));
     }
 
     @EventHandler
     private void onBellRing(BlockDropItemEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_drop));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_drop));
     }
 
     @EventHandler
     private void onReceiveGame(BlockReceiveGameEvent event) {
-        event.setCancelled(isCancelled(event, Flag.sensor));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.sensor));
     }
 
     @EventHandler
     private void onShearEntity(BlockShearEntityEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_shears));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_shears));
     }
 
     @EventHandler
     private void onDispense(BlockDispenseEvent event) {
-        event.setCancelled(isCancelled(event, Flag.block_dispense));
+        event.setCancelled(EventUtils.isBlockEventCancelled(event, Flag.block_dispense));
     }
 
     @EventHandler
     private void onExp(BlockExpEvent event) {
-        if (isCancelled(event, Flag.block_dispense)) event.setExpToDrop(0);
-    }
-
-    public boolean isCancelled(BlockEvent event, Flag flag) {
-        Region region = RegionManager.getRegion(event.getBlock().getWorld(), event.getBlock().getLocation());
-        return region != null && region.hasFlag(flag);
+        if (EventUtils.isBlockEventCancelled(event, Flag.block_dispense)) event.setExpToDrop(0);
     }
 }
---
+++ src/main/java/net/tonimatasdev/fastregions/util/EventUtils.java
@@ -0,0 +1,20 @@
+package net.tonimatasdev.fastregions.util;
+
+import net.tonimatasdev.fastregions.api.flag.Flag;
+import net.tonimatasdev.fastregions.api.region.Region;
+import net.tonimatasdev.fastregions.api.region.RegionManager;
+import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.vehicle.VehicleEvent;
+
+public class EventUtils {
+
+    public static boolean isBlockEventCancelled(BlockEvent event, Flag flag) {
+        Region region = RegionManager.getRegion(event.getBlock().getWorld(), event.getBlock().getLocation());
+        return region != null && region.hasFlag(flag);
+    }
+
+    public static boolean isVehicleEventCancelled(VehicleEvent event, Flag flag) {
+        Region region = RegionManager.getRegion(event.getVehicle().getWorld(), event.getVehicle().getLocation());
+        return region != null && region.hasFlag(flag);
+    }
+}
